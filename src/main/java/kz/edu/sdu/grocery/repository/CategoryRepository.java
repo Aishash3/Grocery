12489c8645642db2c3eab5a9c1bad31be652f2ce
aishashlyk <aishashlik01@gmail.com>
2023-05-17 08:03:18 +0600

    1.TODO: adding Category by name and id

--- src/main/java/kz/edu/sdu/grocery/repository/CategoryRepository.java
+++ src/main/java/kz/edu/sdu/grocery/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package kz.edu.sdu.grocery.repository;
 import kz.edu.sdu.grocery.model.entity.Category;
 import kz.edu.sdu.grocery.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,7 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category getCategoryByName(String categoryName);
+    @Query(nativeQuery = true, value = "SELECT MAX(ID) FROM CATEGORY")
+    Long getMaxId();
 
 }
--- src/main/java/kz/edu/sdu/grocery/service/CategoryService.java
+++ src/main/java/kz/edu/sdu/grocery/service/CategoryService.java
@@ -1,10 +1,19 @@
 package kz.edu.sdu.grocery.service;
 
-import kz.edu.sdu.grocery.model.entity.Customer;
+import kz.edu.sdu.grocery.model.CategoryMapper;
+import kz.edu.sdu.grocery.model.DTO.CategoryDTO;
+import kz.edu.sdu.grocery.model.DTO.CustomerDTO;
 import kz.edu.sdu.grocery.model.entity.Category;
+import kz.edu.sdu.grocery.model.entity.Customer;
 import kz.edu.sdu.grocery.repository.CategoryRepository;
+import kz.edu.sdu.grocery.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import java.util.List;
 
@@ -13,8 +22,52 @@ public class CategoryService {
     @Autowired
     CategoryRepository categoryRepository;
 
-    public Category getCategoryByName(String categoryName) {
-        return categoryRepository.getCategoryByName(categoryName);
+    @Autowired
+    private OrderRepository orderRepository;
+    private CategoryMapper mapper = CategoryMapper.MAPPER;
+    public List<CategoryDTO> getAllCategory() {
+        List<Category> employees = categoryRepository.findAll();
+        return employees.stream().map(mapper::toCategoryDTO).toList();
     }
+
+    //public Category getCategoryByName(String categoryName) {
+      //  return categoryRepository.getCategoryByName(categoryName);
+
+    //}
+    public CategoryDTO getCategoryByName(String name) {
+        return mapper.toCategoryDTO(categoryRepository.getCategoryByName(name));
+    }
+
+    public void createCategory(CategoryDTO categoryDTO) {
+        Category category = mapper.toCategory(categoryDTO);
+        category.setId(getLastId());
+        Category category1 = categoryRepository.save(category);
+    }
+
+    public Long getLastId() {
+        return categoryRepository.getMaxId();
+    }
+
+    //public Double getCategorySum(String name) {
+        //Category category = categoryRepository.getCategoryByName(name);
+        //Long id = category.getId();
+        //List<Object[]> list = categoryRepository.getMaxId(id);
+        //double total = 0L;
+        //for (Object[] row : list) {
+       //     Double id = (Double) row[0];
+        //    total += id;
+        //}
+        //return total;
+    //}
+
+    //public Customer updateCustomer(CustomerDTO customerDTO, Long id) {
+    //    Customer customer = customerRepository.findById(id).
+    //            orElseThrow(() -> new RuntimeException("Such customer doesn't exist"));
+    //    Customer updatedCustomer = mapper.toCustomer(customerDTO);
+    //    updatedCustomer.setId(id);
+    //   return customerRepository.save(updatedCustomer);
+    //}
+
+
 }
 
